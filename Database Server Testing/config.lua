579a440582165c64f9d016e2b848d174a626fe05
Aidan Radinsky <asradinsky@gmail.com>
2019-02-23 10:43:31 -0500

    Update config.lua

--- Database Server Testing/config.lua
+++ Database Server Testing/config.lua
@@ -2,22 +2,16 @@
 -- For more information on config.lua see the Project Configuration Guide at:
 -- https://docs.coronalabs.com/guide/basics/configSettings
 --
+local aspectRatio = display.pixelHeight / display.pixelWidth
+application = {
+   content = {
+      width = aspectRatio > 1.5 and 800 or math.ceil( 1200 / aspectRatio ),
+      height = aspectRatio < 1.5 and 1200 or math.ceil( 800 * aspectRatio ),
+      scale = "letterBox",
+      fps = 30,
 
-application =
-{
-	content =
-	{
-		width = 320,
-		height = 480, 
-		scale = "letterbox",
-		fps = 60,
-		
-		--[[
-		imageSuffix =
-		{
-			    ["@2x"] = 2,
-			    ["@4x"] = 4,
-		},
-		--]]
-	},
+      imageSuffix = {
+         ["@2x"] = 1.3,
+      },
+   },
 }
